253ea8cf236733f08c931923b6dddceefda4b209
PabloTutorMoegle <102219711+PabloTutorMoegle@users.noreply.github.com>
2024-04-22 20:01:01 +0200

    Update startScript.js

--- startScript.js
+++ startScript.js
@@ -1,6 +1,6 @@
 const typedText = document.getElementById('typedText');
 const blinkingSymbol = document.getElementById('blinkingSymbol');
-const text = "Click START to continue."; // The text to be typed (replace with your full text)
+const text = "START"; // The text to be typed (replace with your full text)
 let i = 0;
 
 function typeWriter() {
