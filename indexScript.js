8425142d9d554c1829fb471c1b76f9b5d99d57ed
Pablo de Vicente-Tutor Moegle <pablotutormoegle@gmail.com>
2024-04-25 13:29:54 +0200

    edit 30

--- indexScript.js
+++ indexScript.js
@@ -1,6 +1,6 @@
 const typedText = document.getElementById('typedText');
 const blinkingSymbol = document.getElementById('blinkingSymbol');
-const text = "Click START to continue."; // The text to be typed (replace with your full text)
+const text = "Click START to continue or"; // The text to be typed (replace with your full text)
 let i = 0;
 
 function typeWriter() {
@@ -14,5 +14,23 @@ function typeWriter() {
 typeWriter();
 
 typedText.addEventListener('click', function() {
-  window.location.href = "mainView.html"; /* "./mainView" works for github "mainView.html" works for visual */
+  window.location.href = "./mainView"; /* "./mainView" works for github "mainView.html" works for visual */
 });
+
+const userInput = document.getElementById('userInput');
+const resultContainer = document.getElementById('resultContainer');
+
+userInput.addEventListener('keyup', handleUserInput); 
+
+function handleUserInput(event) {
+  if (event.keyCode === 13) { 
+    const inputText = userInput.value.toLowerCase().trim();
+    const triggerText = 'start';
+
+    if (inputText === triggerText) {
+      window.location.href = './mainView';
+    } else {
+      resultContainer.textContent = 'Try again typing "start".';
+    }
+  }
+}
